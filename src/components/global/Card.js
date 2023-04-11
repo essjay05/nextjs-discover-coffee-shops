05e01e5d2fdfb2025ebe759261c9af2cba87f822
Joy Serquiña <stepjoyous@gmail.com>
2023-04-11 11:12:11 -0700

    Updated Card styling and created global class style of glass.

--- src/components/global/Card.js
+++ src/components/global/Card.js
@@ -5,14 +5,20 @@ import styles from './Card.module.css'
 const Card = (props) => {
   return (
     <Link
-      href={props.href}>
-      <div className={styles.cardContainer}>
-        <h2>{props.name}</h2>
-        <Image
-          src={props.imgSrc}
-          width={260}
-          height={160} 
-          alt={`Image of ${props.name} shop`}/>
+      href={props.href}
+      className={styles.cardLink}>
+      <div className={`glass ${styles.cardContainer}`}>
+        <div className={styles.cardTextWrapper}>
+          <h2 className={styles.cardHeader}>{props.name}</h2>
+        </div>
+        <div className={styles.cardImgWrapper}>
+          <Image
+            src={props.imgSrc}
+            width={260}
+            height={160} 
+            alt={`Image of ${props.name} shop`}
+            className={styles.cardImg}/>
+        </div>
       </div>
     </Link>
     
