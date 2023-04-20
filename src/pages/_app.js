11eef15ffee989f7908cefcd7db1a9dc63e9989e
Joy Serquiña <stepjoyous@gmail.com>
2023-04-19 17:25:48 -0700

    Implemented storeReducer and actions to be used with useContext.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,35 @@
-import { createContext } from 'react'
+import { createContext, useReducer } from 'react'
 import '@/styles/globals.css'
 
 const StoreContext = createContext()
+
+const ACTION_TYPES = {
+  SET_LAT_LONG: 'SET_LAT_LONG',
+  SET_COFFEE_STORES: 'SET_COFFEE_STORES'
+}
+
+const storeReducer = ( state, action ) => {
+  switch(action.type) {
+    case ACTION_TYPES.SET_LAT_LONG: {
+
+    }
+    case ACTION_TYPES.SET_COFFEE_STORES: {
+
+    }
+    default:
+      throw new Error(`Unhandled actiontype: ${actionl.type}`)
+  }
+}
 
 const StoreProvider = ({ children }) => {
   const initialState = {
     latLong: '',
     coffeStores: []
   }
+
+  const [ state, dispatch ] = useReducer(storeReducer, initialState)
   return (
-    <StoreContext.Provider value={{state: initialState}}>
+    <StoreContext.Provider value={{state, dispatch}}>
       { children }
     </StoreContext.Provider>
   )
