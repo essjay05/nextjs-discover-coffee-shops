13391170498372676a0ba754fda16fe87f22d08d
Joy Serquiña <stepjoyous@gmail.com>
2023-04-25 16:34:48 -0700

    Building createCoffeeStore get and post methods wip.

--- src/pages/api/createCoffeeStore.js
+++ src/pages/api/createCoffeeStore.js
@@ -7,8 +7,24 @@ const table = base('coffee-stores')
 
 console.log({table})
 
-const createCoffeeStore = (req, res) => {
-  res.json({ message: 'Creating coffee store'})
+const createCoffeeStore = async(req, res) => {
+  console.log({req})
+  
+  if (req.method === 'POST') {
+    // find a record
+    const findCoffeeStoreRecord = await table.select({
+      filterByFormula: `id="0"`
+    }).firstPage()
+
+    if (findCoffeeStoreRecord.length !== 0) {
+      res.json(findCoffeeStoreRecord)
+    } else {
+      // create record
+      res.json({ message: 'Record not found so CREATE a record'})
+    }
+  } else {
+    res.json({ message: 'This is a get request'})
+  }
 }
 
 export default createCoffeeStore
