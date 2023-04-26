de012bb482300c03da1d69e69f8df492aed81132
Joy Serquiña <stepjoyous@gmail.com>
2023-04-26 15:04:50 -0700

    Building out functionality for creating a coffeeStore using the post route and if the coffeeStore id DOES exist then to return it.

--- src/pages/api/createCoffeeStore.js
+++ src/pages/api/createCoffeeStore.js
@@ -12,24 +12,44 @@ const createCoffeeStore = async(req, res) => {
   
   if (req.method === 'POST') {
     // find a record
-    const findCoffeeStoreRecords = await table.select({
-      filterByFormula: `id="0"`
-    }).firstPage()
-
-    if (findCoffeeStoreRecords.length !== 0) {
-      const csFields = findCoffeeStoreRecords.map(record => {
-        return {
-          ...record.fields
-
-        }
-      })
-      res.json(csFields)
-    } else {
-      // create record
-      res.json({ message: 'Record not found so CREATE a record'})
+    try {
+      const findCoffeeStoreRecords = await table.select({
+        filterByFormula: `id="2"`
+      }).firstPage()
+  
+      if (findCoffeeStoreRecords.length !== 0) {
+        const csFields = findCoffeeStoreRecords.map(record => {
+          return {
+            ...record.fields
+  
+          }
+        })
+        res.json(csFields)
+      } else {
+        // create record
+        const newRecord = await table.create([
+          {
+            'fields': {
+              'id': '2',
+              'name': 'Test Coffee',
+              'address': '2332 42nd Ave, San Francisco, CA 94121',
+              'neighbourhood': 'Outer Richmond SF',
+              'voting': 200,
+              'img-url': 'https://unsplash-img.com/test-coffee'
+            }
+          }
+        ])
+        res.json({ message: 'Created a record', records: newRecord })
+      }
+    } catch(err) {
+      console.error('Error finding the store', err)
+      res.status(500)
+      res.json({ message: 'Error in finding a record.', err})
     }
+    
   } else {
     res.json({ message: 'This is a get request'})
+    
   }
 }
 
