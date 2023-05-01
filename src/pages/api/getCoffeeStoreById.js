847f17a134a789c04998cbbfc9df4e80f0be63e4
Joy Serquiña <stepjoyous@gmail.com>
2023-04-30 21:20:40 -0700

    Created api endpoint getCoffeeStoreById.

--- src/pages/api/getCoffeeStoreById.js
+++ src/pages/api/getCoffeeStoreById.js
@@ -1,8 +1,24 @@
-const getCoffeeStoreById = (req, res) => {
+import { table, getMinifiedRecords } from "@/lib/airtable"
+
+const getCoffeeStoreById = async (req, res) => {
   const { id } = req.query
 
   try {
-    res.json({ message: `id is created ${id}`})
+    if (id) {
+      const findCoffeeStoreRecords = await table.select({
+        filterByFormula: `id="${id}"`
+      }).firstPage()
+  
+      if (findCoffeeStoreRecords.length !== 0) {
+        const record = getMinifiedRecords(findCoffeeStoreRecords)
+        res.json(record)
+      } else {
+        res.json({ message: 'Id could not be found'})
+      }
+    } else {
+      res.status(400)
+      res.json({ message: 'Error: Id is missing' })
+    }
   } catch(err) {
     res.status(500)
     res.json({message: 'Something went wrong fetching the id.', err})
