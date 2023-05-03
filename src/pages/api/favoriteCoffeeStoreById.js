5de66b9548769a007c02eea39b7d8c160cd40a1f
Joy Serquiña <stepjoyous@gmail.com>
2023-05-03 16:44:16 -0700

    Created error handling for favoriteCoffeeStoreById and tested successful return of found id record.

--- src/pages/api/favoriteCoffeeStoreById.js
+++ src/pages/api/favoriteCoffeeStoreById.js
@@ -1,51 +1,32 @@
 import { table, getMinifiedRecords, findRecordByFilter } from "@/lib/airtable"
 
-const favoriteCoffeeStoreById = (req, res) => {
+const favoriteCoffeeStoreById = async (req, res) => {
   
   if (req.method === 'PUT') {
-    // find a record
-    const { id } = req.body
-    res.json({ message: 'this is working', id })
+    try {
+      // find a record
+      const { id } = req.body
     
-    // try {
-    //   if (id) {
-    //     const records = await findRecordByFilter(id)
-    
-    //     if (records.length !== 0) {
-    //       res.json(records)
-    //     } else {
-    //       if (name) {
-    //         // create record
-    //         // res.json({ message: 'Record not found so CREATE a record'})
-    //         const newRecord = await table.update([
-    //           {
-    //             fields: {
-    //               id,
-    //               name,
-    //               address,
-    //               neighbourhood,
-    //               voting,
-    //               imgUrl
-    //             }
-    //           }
-    //         ])
-    //         const records = getMinifiedRecords(newRecord)
-    //         res.status(200)
-    //         res.json(records)
-    //       } else {
-    //         res.status(400)
-    //         res.json({ message: 'Error: Name is missing' })
-    //       }
-    //     }
-    //   } else {
-    //     res.status(400)
-    //     res.json({ message: 'Error: Id is missing' })
-    //   }
-    // } catch(err) {
-    //   console.error('Error in creating or finding the store', err)
-    //   res.status(500)
-    //   res.json({ message: 'Error in creating or finding a record.', err})
-    // }
+      if (id) {
+        const records = await findRecordByFilter(id)
+
+        if (records.length !== 0) {
+          res.json(records)
+        } else {
+          res.status(400)
+          res.json({ message: `Coffee store id: ${id} doesn't exist.`})
+        }
+        res.json({ message: 'this is working', id })
+      } else {
+        res.status(500)
+        res.json({ message: "Error: id is missing"})
+      }
+      
+    } catch(err) {
+      console.error('Error favoriting coffee store', err)
+      res.status(500)
+      res.json({ message: 'Error favoriting coffee store', err})
+    }
   }
 }
 
