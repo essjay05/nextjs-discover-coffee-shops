aace4669df3280b6596bdc5ddf647bb4c084ce87
Joy Serquiña <stepjoyous@gmail.com>
2023-05-05 18:16:52 -0700

    Removed redundant coffee store title. Fixed individual coffee store page mobile responsive styling.

--- src/pages/coffee-store/[id].js
+++ src/pages/coffee-store/[id].js
@@ -120,7 +120,6 @@ const CoffeeStore = (initialProps) => {
     }
   }, [data])
 
-  const title = 'Coffee Store Page'
   const defaultImgUrl = "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
   const iconBaseString = '/static/icons/'
 
@@ -170,7 +169,7 @@ const CoffeeStore = (initialProps) => {
             </div>
             <div className={styles.nameWrapper}>
               <h1 className={styles.storeTitle}>
-                { title }: {name}
+                {name}
               </h1>
             </div>
             <Image
