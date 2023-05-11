e7b5e239d7d8e9937723a375cf97f1aaccab114c
Joy Serquiña <stepjoyous@gmail.com>
2023-05-10 17:10:35 -0700

    Fixed some lint errors for utils/index and pages/index.

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,7 +57,7 @@ export default function Home( props ) {
       }
     }
     setCoffeeStoresByLocation()
-  },[latLong])
+  },[latLong, dispatch])
 
   const handleOnBannerBtnClick = () => {
     handleTrackLocation()
