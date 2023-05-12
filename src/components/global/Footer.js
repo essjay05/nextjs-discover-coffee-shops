ab242823c9d43c2d3debf5f479c92856659889d1
Joy Serquiña <stepjoyous@gmail.com>
2023-05-11 22:01:29 -0700

    Create copyright link to my portfolio website.

--- src/components/global/Footer.js
+++ src/components/global/Footer.js
@@ -1,7 +1,15 @@
+import Link from "next/link"
+import styles from "./Footer.module.css"
+
 const Footer = () => {
   return (
     <footer>
-      <p>This is my Footer</p>
+      <Link 
+        target={`_blank`} 
+        href={`https://www.joyserquina.com`}
+        className={styles.copyright}>
+        Joy of All Trades &copy; 2023
+      </Link>
     </footer>
   )
 }
